83a44a6969e58b5ea22e9b7b5454986f05e37f05
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2020-06-23 01:38:04 +0300

    Fix coercion to `Unit` with explicit `Nothing` argument
     #KT-39669 Fixed

---
+++ compiler/testData/diagnostics/tests/inference/coercionToUnit/coercionToUnitWithNothingType.kt
@@ -0,0 +1,22 @@
+// !DIAGNOSTICS: -UNUSED_PARAMETER
+
+fun launch(block: () -> Unit) {}
+fun <T> run(block: () -> T): T = block()
+
+fun test() {
+    launch {
+        run<Nothing> { TODO("") }
+    }
+    launch {
+        run { TODO("") }
+    }
+    launch {
+        run<String> { "" }
+    }
+    launch {
+        run<Nothing?> { null }
+    }
+    launch {
+        run { <!UNUSED_EXPRESSION!>null<!> }
+    }
+}
